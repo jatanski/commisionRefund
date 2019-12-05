3063506ee7453f7e12eda59e37462a338ce0c631
Kuba <ja.tanski@gmail.com>
2019-12-05 16:49:58 +0100

    change seo file

---
+++ src/pages/reklama2.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../scss/pages/reklama2.scss';
+import Img from '../components/reklama/img1';
+
+const Reklama = () => {
+  return (
+    <section className="reklama2">
+      <div className="img">
+        <Img></Img>
+      </div>
+      <div className="text">
+        <p className="text__paraf">
+          Odzyskujemy pieniądze z wcześniej spłaconych i <span className="bg-yellow">skonsolidowanych</span> kredytów.
+          Nawet 12 000 zł w 5 dni od kontaktu!
+        </p>
+      </div>
+      <div className="url">
+        <p className="url__text">https://odzyskamytwojaprowizje.pl</p>
+      </div>
+    </section>
+  );
+};
+
+export default Reklama;
